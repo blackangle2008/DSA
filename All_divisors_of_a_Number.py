c19fa7e8c046c21fde9b71d5ab2c5b5c0b9f5bef
blackangle2008 <sujal12012008@gmail.com>
2026-03-17 16:45:03 +0530

    Code of all divisors of a number

---
+++ All_divisors_of_a_Number.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+
+divisors = []
+
+for i in range(1, int(n**0.5) + 1):
+    if n % i == 0:
+        divisors.append(i)
+        if i != n // i:
+            divisors.append(n // i)
+
+divisors.sort()
+
+for d in divisors:
+    print(d, end=" ")
