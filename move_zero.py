70224892c7f3e689904f0909e9bf7ce01a7e0e2d
blackangle2008 <sujal12012008@gmail.com>
2026-03-23 09:56:17 +0530

    Code of move zero

---
+++ move_zero.py
@@ -0,0 +1,10 @@
+nums = [0, 1, 0, 3, 12]
+
+j = 0  # position for next non-zero
+
+for i in range(len(nums)):
+    if nums[i] != 0:
+        nums[j], nums[i] = nums[i], nums[j]
+        j += 1
+
+print(nums)
