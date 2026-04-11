6e8ec84d9084ebf050f699e775398e7dbd3f302e
blackangle2008 <sujal12012008@gmail.com>
2026-04-11 08:49:30 +0530

    Code of maximum subarray

---
+++ Max_Subarray.py
@@ -0,0 +1,9 @@
+def maxSubArray(nums):
+    current_sum = nums[0]
+    max_sum = nums[0]
+
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
